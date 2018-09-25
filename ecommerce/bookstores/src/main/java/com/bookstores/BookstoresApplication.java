d7ab75f26b6101186fd202e5ddfa53f454a5803d
dishankgoyal <dishank29goyal@gmail.com>
2018-09-25 00:11:25 -0400

    new code
    when i removed the hibernate code then my user will give me the error.

--- ecommerce/bookstores/src/main/java/com/bookstores/BookstoresApplication.java
+++ ecommerce/bookstores/src/main/java/com/bookstores/BookstoresApplication.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
+
 public class BookstoresApplication {
 
 	public static void main(String[] args) {
